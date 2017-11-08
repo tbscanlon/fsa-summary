6ff6db59150582624c9d9a7ce580db8c10610efb
tom.scanlon <tom.scanlon@kingfisher.com>
2017-11-08 09:28:04 +0000

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { TableComponent } from './table/table.component';
+import { TableRowComponent } from './table-row/table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    TableComponent,
+    TableRowComponent
   ],
   imports: [
     BrowserModule
